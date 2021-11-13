5a8b60222d68df04322adbf083feb3866f0e679c
Miguel Licea <angelmigue.cespedes@gmail.com>
2021-11-12 19:36:47 -0500

    Finished Michael's contribution

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import ply.lex as lex
 
+reserved = {
+    #Parte de Miguel
+    'if':'IF',
+    'else':'ELSE',
+    'for':'FOR',
+    'while':'WHILE',
+    'import':'IMPORT',
+    'print':'PRINT',
+    'int':'INT',
+    'double':'DOUBLE',
+    'String':'STRING',
+    'bool':'BOOL',
+    #Termina parte de Miguel
+}
 # List of token names.   This is always required
 tokens = (
     'NUMBER',
@@ -12,7 +26,31 @@ tokens = (
     'RPAREN',
     'DIVISION_ENTERA',
     'RESIDUO'
-)
+    #Parte de Miguel
+    'MAYORQUE',
+    'NOT',
+    'DIVENTERA',
+    'RESIDUO',
+    'PUNTOCOMA',
+    'LLAVEABRE',
+    'LLAVECIERRA',
+    'COMA',
+    'DOSPUNTOS',
+    #Termina parte de Miguel
+)+ tuple(reserved.values())
+
+#Empiezaa parte de Miguel
+t_MAYORQUE = r'\>'
+t_NOT = r'!'
+t_DIVENTERA = r'\~/'
+t_RESIDUO = r'\%'
+t_PUNTOCOMA = r'\;'
+t_LLAVEABRE = r'\{'
+t_LLAVECIERRA = r'\}'
+t_COMA = r'\,'
+t_DOSPUNTOS = r'\:'
+#Termina parte de Miguel
+
 
 # Regular expression rules for simple tokens
 t_PLUS = r'\+'
@@ -28,8 +66,8 @@ t_RESIDUO = r'%'
 
 
 def t_VARIABLE(t):
-    r'[a-zA-Z$_][\w$_]*'
-    t.value = str(t.value)
+    r'[a-zA-Z$_][a-zA-Z\d$_]*'
+    t.type = reserved.get(t.value,'VARIABLE')
     return t
 
 
@@ -59,15 +97,56 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test it out
-
+#Parte de Miguel
 data = '''
 123
 @asa_AS
 a14$
 15#A
-15~/15
-6%6
+~
+%
+()
+;
+import "dart:io";
+if(){}else{}
+for (){} , : =
+Map<String,int> mapa = { 
+       "lenguajes":10 
+}; 
+mapa.remove("hola");
+mapa.clear();
+
+void saludar (String saludo){ 
+
+print (saludo+ "hola"); 
+
+} 
+int numero() => 4;
 '''
+
+data ='''
+Set numberSet = new  Set(); 
+numberSet.add(100); 
+numberSet.add(200); 
+print(numberSet.join(";"));
+'''
+
+data = '''
+List<int> lista = [1,2,3]; 
+lista.add(5); 
+lista.clear();
+String str = stdin.readLineSync(); 
+a % b 
+A != B 
+'''
+#Pruebas de Operadores de asignacion
+data = '''
+    int x = 4;
+    bool session = true;
+    x+=1;
+    x-=3;
+'''
+#Termina parte de Miguel
 
 # Give the lexer some input
 lexer.input(data)
