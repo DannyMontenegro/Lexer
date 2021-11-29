613262ed361adf5ee8a54d7b5fc811b769575844
Raul Villao <rjvillao@espol.edu.ec>
2021-11-29 17:02:16 -0500

    Metodos léxico, smentico y sintactico
    Metodos para analisi a corde a los botones

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ from main import errores_lexicos, lexer
 from tkinter import *
 raiz = Tk()
 
-def verificar():
+def verificarSintax():
     entrada = textArea.get(1.0,"end-1c")
     #print(entrada.count('\n'))
     #print(entrada)
@@ -13,13 +13,11 @@ def verificar():
     resultado = str(parser.parse(entrada))
     # print("Analizando")
     #print(resultado)
-    if(resultado=='None' and (len(errors)==0) and len(errores_lexicos)==0 and len(errores_semanticos)==0):
+    if(resultado=='None' and (len(errors)==0)):
         varSalida.set("Salida: No hay errores sintácticos en su código")
     else:
-        lexicos = '\n'.join(errores_lexicos)
         sintacticos = '\n'.join(errors)
-        semanticos = '\n'.join(errores_semanticos)
-        salida = "Errores lexicos: "+ lexicos + '\nErrores sintáticos: ' + sintacticos + '\nErrores Semánticos' + semanticos
+        salida = 'Errores sintáticos: ' + sintacticos
         varSalida.set(salida)
         errors.clear()
         errores_lexicos.clear()
@@ -27,20 +25,67 @@ def verificar():
         variables.clear()
         funciones.clear()
         parser.defaulted_states = {}
-        
+
+def verificarLex():
+    entrada = textArea.get(1.0, "end-1c")
+    # print(entrada.count('\n'))
+    # print(entrada)
+    lexer.lineno = 1
+    resultado = str(parser.parse(entrada))
+    if (resultado == 'None' and len(errores_lexicos)==0):
+        varSalida.set("Salida: No hay errores Léxicos en su código")
+    else:
+        lexicos = '\n'.join(errores_lexicos)
+        salida =salida = "Errores lexicos: "+ lexicos + '\n'
+        varSalida.set(salida)
+        errores_lexicos.clear()
+        variables.clear()
+        funciones.clear()
+        parser.defaulted_states = {}
+
+
+def verificarSem():
+    entrada = textArea.get(1.0, "end-1c")
+    # print(entrada.count('\n'))
+    # print(entrada)
+    lexer.lineno = 1
+    resultado = str(parser.parse(entrada))
+    if (resultado == 'None' and len(errores_semanticos) == 0):
+        varSalida.set("Salida: No hay errores semánticos en su código")
+    else:
+        semanticos = '\n'.join(errores_semanticos)
+        salida = salida = 'Errores Semánticos: ' + semanticos
+        varSalida.set(salida)
+        errores_semanticos.clear()
+        variables.clear()
+        funciones.clear()
+        parser.defaulted_states = {}
 
 raiz.title("Proyecto LP")
-raiz.geometry("500x500")
+raiz.geometry("700x500")
 
 var = StringVar()
 label = Label( raiz, textvariable=var, relief=RAISED )
 
-var.set("ANALIZADOR SINTACTICO")
+var.set("DART")
 label.pack()
 textArea = Text(raiz,width=40,height=15)
 textArea.pack()
 
-Button(raiz,text="Analisis Sintactico",command=verificar).pack()
+Button(raiz,text="Analisis Sintactico",command=verificarSintax).pack()
+varSalida = StringVar()
+labelSalida = Label( raiz, textvariable=varSalida,)
+varSalida.set("Salida:")
+labelSalida.pack()
+
+
+Button(raiz,text="Analisis Léxico",command=verificarLex).pack()
+varSalida = StringVar()
+labelSalida = Label( raiz, textvariable=varSalida,)
+varSalida.set("Salida:")
+labelSalida.pack()
+
+Button(raiz,text="Analisis Semántico",command=verificarSem).pack()
 varSalida = StringVar()
 labelSalida = Label( raiz, textvariable=varSalida,)
 varSalida.set("Salida:")
